7890f32e768e67a24f51be7329a8b7be208fb563
Jun <junaidvayoli@gmail.com>
2020-06-28 02:29:13 +0900

    ideal RC

--- start.py
+++ start.py
@@ -13,9 +13,9 @@ from time import sleep
 import os
 from time import time
 
-BOT_TOKEN = os.environ['COVID_BOT_TOKEN']
-if os.environ['UPDATE_ID']:
-    update_id = int(os.environ['UPDATE_ID'])
+BOT_TOKEN = os.environ["COVID_BOT_TOKEN"]
+if os.environ["UPDATE_ID"]:
+    update_id = int(os.environ["UPDATE_ID"])
 else:
     update_id = None
 print("this is the last update_id")
@@ -24,7 +24,6 @@ print(update_id)
 
 start_time = int(time())
 print("Start time: " + str(start_time))
-
 
 
 def main():
@@ -40,16 +39,9 @@ def main():
     except IndexError:
         update_id = 0
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     while True:
-        if int(time()) - start_time > 60:
-            print("enough for the day!")
-            with open('/tmp/update_id', 'w') as the_file:
-                the_file.write(str(update_id))
-            break
-        else:
-            print(int(time()) - start_time)
         try:
             echo(bot)
         except NetworkError:
@@ -57,13 +49,20 @@ def main():
         except Unauthorized:
             # The user has removed or blocked the bot.
             update_id += 1
+        if int(time()) - start_time > 3600:
+            print("enough for the day!")
+            with open("/tmp/update_id", "w") as the_file:
+                the_file.write(str(update_id))
+            break
+        else:
+            print(int(time()) - start_time)
 
 
 def echo(bot):
     """Echo the message the user sent."""
     global update_id
     # Request updates after the last update_id
-    for update in bot.get_updates(offset=update_id, timeout=10):
+    for update in bot.get_updates(offset=update_id, timeout=60):
         update_id = update.update_id + 1
         # print(update)
 
@@ -72,5 +71,5 @@ def echo(bot):
             update.message.reply_text(update.message.text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
