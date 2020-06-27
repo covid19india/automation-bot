193432acd20c9fd3546b9ad65f650c35257f1c68
Jun <junaidvayoli@gmail.com>
2020-06-28 02:13:42 +0900

    connecting events

--- start.py
+++ start.py
@@ -14,12 +14,15 @@ import os
 from time import time
 
 BOT_TOKEN = os.environ['COVID_BOT_TOKEN']
+UPDATE_ID = os.environ['UPDATE_ID']
+print("this is the last update_id")
+print(UPDATE_ID)
 # print(BOT_TOKEN)
 
 start_time = int(time())
 print("Start time: " + str(start_time))
 
-update_id = None
+update_id = 0
 
 
 def main():
@@ -33,17 +36,19 @@ def main():
     try:
         update_id = bot.get_updates()[0].update_id
     except IndexError:
-        update_id = None
+        update_id = 0
 
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     while True:
+        if int(time()) - start_time > 60:
+            print("enough for the day!")
+            with open('/tmp/update_id', 'w') as the_file:
+                the_file.write(str(update_id))
+            break
+        else:
+            print(int(time()) - start_time)
         try:
-            if int(time()) - start_time > 60:
-                print("enough for the day!")
-                break
-            else:
-                print(int(time()) - start_time)
             echo(bot)
         except NetworkError:
             sleep(1)
